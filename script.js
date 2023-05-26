70301a9740c4ea516ef35006c1dd385a6dff674a
vasil <vasil.gmail.com>
2023-05-26 16:07:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.burger-bar').addEventListener('click', function () {
+    document.querySelector('.block-burger-bar').style.display = 'flex';
+})
+
+document.querySelector('.block-burger-bar').addEventListener('click', function () {
+    document.querySelector('.block-burger-bar').style.display = 'none';
+})
